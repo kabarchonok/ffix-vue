107ac939a4a13e1e8a9364e965ef14e1e37cc7cf
ivan.b <ivan.b@aio.media>
2019-05-07 18:58:58 +0500

    added help button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="footer">
-        <div v-if="page !== 'main'" class="action" @click="goToMain()">Back</div>
+        <div v-if="page !== 'main'"  class="action action-back" @click="goToMain()">Back</div>
+        <div class="action action-help">? Help</div>
     </div>
 </template>
 
@@ -26,9 +27,11 @@
         grid-row: 10;
         background-image: url("../assets/container-bg.png");
         position: relative;
+        padding: 0 15px;
+        border: 3px solid #717479;
         border-top: 5px ridge #d3d5da;
-        display: flex;
-        justify-content: center;
+        border-right-color: #3a3b43;
+        border-bottom-color: #3a3b43;
     }
 
     .action {
@@ -38,6 +41,19 @@
         border: 3px solid #717479;
         border-right-color: #3a3b43;
         border-bottom-color: #3a3b43;
-        padding: 11px 0;
+        padding: 11px 0 12px;
+    }
+
+    .action-back {
+        width: 200px;
+        float: left;
+    }
+
+    .action-help {
+        float: right;
+    }
+
+    .action:active {
+        background: linear-gradient(to bottom, rgba(255, 255, 255, 0.14) 35%, transparent);
     }
 </style>
