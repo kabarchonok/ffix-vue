f5f3243665addcff112001adcb4f2eefafecf775
ivan.b <ivan.b@aio.media>
2019-05-07 13:53:42 +0500

    added go back button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="footer"></div>
+    <div class="footer">
+        <div v-if="page !== 'main'" class="action" @click="goToMain()">Back</div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Footer"
+        name: "Footer",
+        computed: {
+            page() {
+                return this.$store.state.page
+            }
+        },
+        methods: {
+            goToMain() {
+                this.$store.commit('updatePage', 'main');
+            }
+        }
     }
 </script>
 
@@ -15,5 +27,17 @@
         background-image: url("../assets/container-bg.png");
         position: relative;
         border-top: 5px ridge #d3d5da;
+        display: flex;
+        justify-content: center;
+    }
+
+    .action {
+        text-align: center;
+        width: 150px;
+        cursor: pointer;
+        border: 3px solid #717479;
+        border-right-color: #3a3b43;
+        border-bottom-color: #3a3b43;
+        padding: 11px 0;
     }
 </style>
